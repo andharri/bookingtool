1ce811f549b567ace72af0decec673b318634461
Cloud User <centos@euw2-anorien-1.ghco.co.uk>
2022-09-12 15:06:25 +0000

    no comment

--- ebt-roy/roy.py
+++ ebt-roy/roy.py
@@ -1,4 +1,8 @@
 import logging
+import requests
+import json
+
+## setup logging
 
 logging.basicConfig(
 	format='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s',
@@ -7,3 +11,26 @@ logging.basicConfig(
 log = logging.getLogger("logger")
 
 log.info("ROY")
+
+## get auth token
+
+body = {"username":"admin","password":"password"}
+
+response = requests.post("http://10.4.104.171:8080/Services/Authentication/Authenticate", data=json.dumps(body))
+
+print(json.loads(response.text)['sessionToken'])
+
+authToken = json.loads(response.text)['sessionToken']
+
+## get list of labs
+
+header = {
+	"X-Booked-SessionToken": authToken,
+	"X-Booked-UserId": 2
+}
+print(header)
+
+response = requests.get("http://10.4.104.171:8080/Services/Resources/GetAllResources", headers=header)
+
+print(response.text)
+
