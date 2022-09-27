b0949d8f380c59cd850d04bd5361bac33b925e0d
Cloud User <centos@euw2-anorien-1.ghco.co.uk>
2022-09-27 12:56:43 +0000

    no comment

--- ebt-roy/roy.py
+++ ebt-roy/roy.py
@@ -1,6 +1,35 @@
 import logging
 import requests
 import json
+import pprint
+import time
+
+def isLabPoweredOn(lab):
+	#TODO
+	log.info("lab: "+lab+" is powered on!")
+	return False
+	
+def powerOnLab(lab):
+	log.info("Powering on: "+lab)
+	#TODO
+	log.info("Lab is powered on: "+lab)
+	
+def powerOffLab(lab):
+	log.info("Powering off: "+lab)
+	#TODO
+	log.info("Lab is powered off: "+lab)
+
+def isBookingActive(startTime,endTime):
+	currentEpoch = time.time()
+	startTimeEpoch = time.mktime(time.strptime(startTime, "%Y-%m-%dT%H:%M:%S+0000"))
+	endTimeEpoch = time.mktime(time.strptime(endTime, "%Y-%m-%dT%H:%M:%S+0000"))
+	
+	if currentEpoch < endTimeEpoch and currentEpoch > startTimeEpoch:
+		log.info("Booking is active")
+		return True
+	else:
+		log.info("Booking is not active")
+		return False
 
 ## setup logging
 
@@ -15,25 +44,44 @@ log.info("ROY")
 ## get auth token
 
 body = {"username":"admin","password":"password"}
-
 response = requests.post("http://10.4.104.171:8080/Services/Authentication/Authenticate", data=json.dumps(body))
-
-print(json.loads(response.text)['sessionToken'])
-
 authToken = json.loads(response.text)['sessionToken']
 
 ## get list of labs
 
+labs = []
 header = {
 	"X-Booked-SessionToken": authToken,
 	"X-Booked-UserId": "2"
 }
-print(header)
 
 response = requests.get("http://10.4.104.171:8080/Services/Resources/", headers=header)
 
-#print(response.text)
-
 for x in json.loads(response.text)['resources']:
-	print(x)
+	lab = {
+		'id': x['resourceId'],
+		'name': x['name']
+	}
+	labs.append(lab)
 
+## for each lab work out if we are in an active booking
+
+for lab in labs:
+
+	response = requests.get("http://10.4.104.171:8080/Services/Reservations/?resourceId="+lab['id'], headers=header)
+	
+	for x in json.loads(response.text)['reservations']:
+		if isBookingActive(x['startDate'],x['endDate']):
+			# check that lab is powered on or not
+			if not isLabPoweredOn(lab['name']):
+				#power on lab
+				powerOnLab(lab['name'])
+		elif isBookingSoon(x['startDate'],x['endDate']):
+			if isLabPoweredOn(lab['name']):
+				resetLabConfig(lab['name'])
+			else:
+				powerOnLab(lab['name'])
+		else:
+			if isLabPoweredOn(lab['name']):
+				powerOffLab(lab['name'])
+				
